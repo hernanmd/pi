2ced515540f70583a71c76db05d514e203cdaadc
hernanmd <hernan.morales@gmail.com>
2019-10-17 21:03:14 -0300

    Updated version and date strings

--- libexec/piEnvVars.sh
+++ libexec/piEnvVars.sh
@@ -7,8 +7,8 @@
 ## Pharo Installer Settings
 #################################
 
-piVersion="0.4.1"
-piDate="12/02/2019"
+piVersion="0.4.2"
+piDate="17/10/2019"
 cacheDir=$HOME/.pi-cache
 imageName="Pharo.image"
 zeroConfUrl="https://get.pharo.org"
