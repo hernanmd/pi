e80455dc2af01421fe9f8f7daa94f2321f504fda
hernanmd <hernan.morales@gmail.com>
2020-01-25 00:30:36 -0300

    Add deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,15 +8,15 @@ set -eo pipefail
 main () {
     local version
 
-    if [ $# -eq 0 ]; then
-        echo "No arguments supplied"
-        exit 1
-    fi
-    version="$1"
-
     echo $(date "+%d-%m-%Y") > DATE
     [[ -f DATE ]] || { echo "Couldn't write DATE file for release"; exit 1; }
-    release-it "$version"
+
+    if [ $# -eq 0 ]; then
+        release-it
+    else
+        version="$1"
+        release-it "$version"
+    fi
 }
 
 main $*
